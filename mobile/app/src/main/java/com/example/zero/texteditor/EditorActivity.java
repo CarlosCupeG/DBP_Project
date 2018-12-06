3d74e6e9ed55e6ea47572e6e7b3e132c3625dff3
CarlosCupeG <carlosdaniel922@live.com>
2018-12-06 11:17:45 -0500

    Correcciones

--- mobile/app/src/main/java/com/example/zero/texteditor/EditorActivity.java
+++ mobile/app/src/main/java/com/example/zero/texteditor/EditorActivity.java
@@ -2,6 +2,21 @@ package com.example.zero.texteditor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -12,6 +27,61 @@ public class EditorActivity extends AppCompatActivity {
     }
 
     public void createChange() {
+        String url = "http://carloscupe.pythonanywhere.com/mobile_change";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        Map<String, String> params = new HashMap();
+        params.put("val", "1");
+        params.put("event", "write");
+        JSONObject parameters = new JSONObject(params);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                parameters,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+                error.printStackTrace();
+
+            }
+        });
+        queue.add(jsonObjectRequest);
+
+
+    }
+
+    public void updateChange() {
+        String url = "http://carloscupe.pythonanywhere.com/mobile_change";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+                error.printStackTrace();
+
+            }
+        });
+        queue.add(jsonObjectRequest);
+
 
     }
 }
